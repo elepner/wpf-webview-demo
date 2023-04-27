ab9ca902e8a82ff292b0a8d4ba3a90bf7a355ffa
Eduard Lepner <eduard@invarion.com>
2023-04-27 17:31:03 +0200

    working Quill editor in WPF

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,28 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        InitializeWebView();
     }
+
+    private async void InitializeWebView()
+    {
+        await MyWebView.EnsureCoreWebView2Async(null);
+        string htmlContent = """"
+            <html>
+
+            <body>
+              <h1>Hello, WebView2!</h1>
+              <div id="editor-container">
+              </div>
+            </body>
+            <style>
+              #editor-container {
+                height: 375px;
+                width: 100%;
+                border: 1px solid cyan;
+              }
+            </style>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.min.js"></script>
+
+            <!-- Theme included stylesheets -->
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.snow.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.bubble.css" rel="stylesheet">
+
+            <script>
+              console.log("Hello world");
+              var quill = new Quill('#editor-container', {
+                modules: {
+                  toolbar: [
+                    [{ header: [1, 2, false] }],
+                    ['bold', 'italic', 'underline'],
+                    ['image', 'code-block']
+                  ]
+                },
+                placeholder: 'Compose an epic...',
+                theme: 'snow'  // or 'bubble'
+              });
+            </script>
+
+            </html>
+            """";
+        
+        MyWebView.CoreWebView2.NavigateToString(htmlContent);
+    }
+
 }
